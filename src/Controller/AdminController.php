aa7db628e1fba4ceadd90d5267f32a9cb07b1676
OddIsBack-212 <damiennicolleau7@gmail.com>
2020-02-20 16:52:45 +0100

    commit gestion sites

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,6 +7,7 @@ use App\Entity\Site;
 use App\Entity\Sortie;
 use App\Entity\Ville;
 use App\Form\AdminSitesUpdateType;
+use App\Form\FiltreSiteType;
 use App\Form\SortieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -33,16 +34,15 @@ class AdminController extends AbstractController
      * @param EntityManagerInterface $em
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function adminSites(EntityManagerInterface $em,Request $request)
+    public function adminSites(EntityManagerInterface $em)
     {
+        $filtreSiteForm=$this->createFormBuilder(FiltreSiteType::class);
         $repsites = $em->getRepository(Site::class);
-        $sites=$repsites->findAll();
-        $repsites = $em->getRepository(Site::class);
-        $sitesTrier=$repsites->findAll();
-
-        return $this->render('admin/adminSites.html.twig', ["sites"=>$sites,
-            'sitesTriers'=>$sitesTrier]);
+        $sites = $repsites->findAll();
+        return $this->render('admin/adminSites.html.twig', ["sites" => $sites]);
     }
+
+
 
     /*
      * @IsGranted("ROLE_ADMIN")
@@ -70,81 +70,23 @@ class AdminController extends AbstractController
     }*/
 
     /**
+     * @param EntityManagerInterface $em
+     * @param $id
+     * @param Request $request
      * @Route("/adminSites/Update/{id}",name="adminSitesUpdate")
      * @IsGranted("ROLE_ADMIN")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
      */
-    public function adminSitesUpdate(EntityManagerInterface $em,$id,Request $request){
+    public function adminSitesUpdate(EntityManagerInterface $em, $id, Request $request)
+    {
 
-        $siteclass= new Site();
-        $form = $this->createForm(AdminSitesUpdateType::class,$siteclass);
+        $siteclass = new Site();
+        $form = $this->createForm(AdminSitesUpdateType::class, $siteclass);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $siteRepository=$em->getRepository(Site::class);
-            $site=$siteRepository->find($id);
-
-            $nomSite = $form->get('nom')->getViewData();
-            $site->setNom($nomSite);
-
-            $em->persist($site);
-
-            $em->flush();
-
-            $this->addFlash("Success","Votre site a bien été modifié !");
-
-            return $this->RedirectToRoute("adminSites");}
-
-        return $this-> render('admin/siteUpdate.html.twig',[
-            'siteForm'=>$form->createView()
-        ]);
-    }
-
-
-    /**
-     * @Route("/adminSites/Delete/{id}",name="adminSitesDelete")
-     * @IsGranted("ROLE_ADMIN")
-     */
-    public function adminSitesDelete(EntityManagerInterface $em,$id){
-
-        $site=$em->getRepository(Site::class)->find($id);
-
-        if(!$id){
-            return $this->RedirectToRoute("main");
-        }
-
-        $em->remove($site);
-        $em->flush();
-
-        $siteRepository = $em->getRepository(Site::class);
-        $sites = $siteRepository->findAll();
-
-        $this->addFlash("Success","Le site a bien été supprimé !");
-
-        return $this-> render('admin/adminSites.html.twig',["sites"=>$sites]);
-    }
-
-    /**
-     * @Route("/adminSortie", name="adminArchivageSortie")
-     * @isGranted("ROLE_ADMIN")
-     */
-    public function archiveSortie (EntityManagerInterface $em){
-
-        $sorties = $em->getRepository(Sortie::class)->findAll();
-
-        return $this->render('admin/adminSortie.html.twig', [
-            'sorties' => $sorties,
-        ]);
-
-    }
-
-    /*
-     * @Route("/adminSites/Add/", name="adminSitesAdd")
-     */
-    public function adminSitesAdd(EntityManagerInterface $em, Request $request){
-        $site= new Site();
-        $form = $this->createForm(AdminSitesUpdateType::class,$site);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+            $siteRepository = $em->getRepository(Site::class);
+            $site = $siteRepository->find($id);
 
             $nomSite = $form->get('nom')->getViewData();
             $site->setNom($nomSite);
@@ -158,9 +100,82 @@ class AdminController extends AbstractController
             return $this->RedirectToRoute("adminSites");
         }
 
-            return $this->render('admin/siteUpdate.html.twig',[
-                'siteForm'=>$form->createView()
-            ]);
+        return $this->render('admin/siteUpdate.html.twig', [
+            'siteForm' => $form->createView()]);
+    }
+
+
+    /**
+     * @param $id
+     * @Route("/adminSites/Delete/{id}",name="adminSitesDelete")
+     * @IsGranted("ROLE_ADMIN")
+     * @return Response
+     */
+    public function adminSitesDelete(EntityManagerInterface $em,$id)
+    {
+
+            $site = $em->getRepository(Site::class)->find($id);
+
+            $site->setArchive(true);
+
+            $em->persist($site);
+
+            $em->flush();
+
+            $this->addFlash("Success", "Le site a bien été supprimé !");
+
+
+
+        return $this->RedirectToRoute('main');
+
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @Route("/adminSortie", name="adminArchivageSortie")
+     * @isGranted("ROLE_ADMIN")
+     * @return Response
+     */
+    public function archiveSortie(EntityManagerInterface $em)
+    {
+
+        $sorties = $em->getRepository(Sortie::class)->findAll();
+
+        return $this->render('admin/adminSortie.html.twig', [
+            'sorties' => $sorties,
+        ]);
+
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @Route("/adminSites/Add/", name="adminSitesAdd")
+     * @isGranted("ROLE_ADMIN")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function adminSitesAdd(EntityManagerInterface $em, Request $request)
+    {
+        $site = new Site();
+        $form = $this->createForm(AdminSitesUpdateType::class, $site);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $site->setArchive(false);
+            $nomSite = $form->get('nom')->getViewData();
+            $site->setNom($nomSite);
+
+            $em->persist($site);
+
+            $em->flush();
+
+            $this->addFlash("Success", "Votre site a bien été modifié !");
+
+            return $this->RedirectToRoute("adminSites");
+        }
+
+        return $this->render('admin/siteUpdate.html.twig', [
+            'siteForm' => $form->createView()
+        ]);
     }
 
 
